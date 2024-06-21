9b5ddb70040ee4311971c0f953eba61eb6875b41
maregames81 <marepersonale@gmail.com>
2024-06-21 17:16:52 +0200

    Inizio progetto (forse)

---
+++ Siw-Barber/src/main/java/it/uniroma3/siw/SiwBarberApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBarberApplication.class, args);
+	}
+
+}
