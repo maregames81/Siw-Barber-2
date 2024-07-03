e30bbd6a5fbd83848eb14542a7c3cc57b34d4281
Bogdan Andrei Tutuianu <128369457+bogX2@users.noreply.github.com>
2024-07-03 17:02:56 +0200

    fine.

--- Siw-Barber/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ Siw-Barber/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -46,7 +46,7 @@ public class AuthConfiguration {
     .authorizeHttpRequests()
     // .requestMatchers("/**").permitAll()
     // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-    .requestMatchers(HttpMethod.GET,"/","/index","/register","/css/**", "/images/**", "favicon.ico").permitAll()
+    .requestMatchers(HttpMethod.GET,"/","/index","/register","/css/**", "/images/**").permitAll()
     // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
     .requestMatchers(HttpMethod.POST,"/register", "/login").permitAll()
     .requestMatchers(HttpMethod.GET,"/admin/**").hasAnyAuthority(ADMIN_ROLE)
